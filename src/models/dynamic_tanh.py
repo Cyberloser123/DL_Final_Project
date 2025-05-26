91777484d544cdb2d2fd62f682317a0fc090a9e1
mzh <a0966905630@gmail.com>
2025-05-26 21:25:33 +0800

    fix: fix DyT bug

--- src/models/dynamic_tanh.py
+++ src/models/dynamic_tanh.py
@@ -4,11 +4,10 @@ from timm.layers import LayerNorm2d
 
 
 class DynamicTanh(nn.Module):
-    def __init__(self, normalized_shape, channels_last, alpha_init_value=0.5):
+    def __init__(self, normalized_shape, alpha_init_value=0.5):
         super().__init__()
         self.normalized_shape = normalized_shape
         self.alpha_init_value = alpha_init_value
-        self.channels_last = channels_last
 
         self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
         self.weight = nn.Parameter(torch.ones(normalized_shape))
@@ -16,10 +15,7 @@ class DynamicTanh(nn.Module):
 
     def forward(self, x):
         x = torch.tanh(self.alpha * x)
-        if self.channels_last:
-            x = x * self.weight + self.bias
-        else:
-            x = x * self.weight[:, None, None] + self.bias[:, None, None]
+        x = x * self.weight + self.bias
         return x
 
     def extra_repr(self):
@@ -29,7 +25,7 @@ class DynamicTanh(nn.Module):
 def convert_ln_to_dyt(module):
     module_output = module
     if isinstance(module, nn.LayerNorm):
-        module_output = DynamicTanh(module.normalized_shape, not isinstance(module, LayerNorm2d))
+        module_output = DynamicTanh(module.normalized_shape)
     for name, child in module.named_children():
         module_output.add_module(name, convert_ln_to_dyt(child))
     del module
@@ -38,7 +34,7 @@ def convert_ln_to_dyt(module):
 def convert_ln_to_dyt_pred(module):
     module_output = module
     if isinstance(module, nn.LayerNorm):
-        module_output = DynamicTanh(module.normalized_shape, module.elementwise_affine)
+        module_output = DynamicTanh(module.normalized_shape)
     for name, child in module.named_children():
         module_output.add_module(name, convert_ln_to_dyt_pred(child))
     del module
